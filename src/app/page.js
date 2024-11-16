2dffe4ec0fc4aba17b6e6db55c81dd646617cd4d
Rodrigo Santos <rrc.santos@campus.fct.unl.pt>
2024-11-16 19:00:24 +0000

    Added noise and the boilerplate for the project sections.

--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,66 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main>
+      <div className={styles.container}>
+        <section className={styles.hero}>
+          <p>Scroll down</p>
+        </section>
+        <section className={styles.sticky}>
+          <div className={styles.col}>
+            <div className={styles.services}>
+              <div className={styles.indicator}></div>
+              <div className={styles.serviceActive}>
+                <p>Service 1</p>
+              </div>
+              <div className={styles.service}>
+                <p>Service 2</p>
+              </div>
+              <div className={styles.service}>
+                <p>Service 3</p>
+              </div>
+              <div className={styles.service}>
+                <p>Service 4</p>
+              </div>
+              <div className={styles.service}>
+                <p>Service 5</p>
+              </div>
+              <div className={styles.service}>
+                <p>Service 5</p>
+              </div>
+              <div className={styles.service}>
+                <p>Service 6</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.col}>
+            <div className={styles.serviceImageWrapper}>
+              <div className={styles.serviceImage}>
+                <Image src="" alt="Service 1" className={styles.image} />
+                <Image src="" alt="Service 1" className={styles.image} />
+              </div>
+            </div>
+            <div className={styles.serviceCopy}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quos.
+              </p>
+            </div>
+          </div>
+          <div className={styles.progressBar}>
+            <div className={styles.progress}></div>
+          </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <div className={styles.index}>
+            <span className={styles.currentCount}>1</span>
+            <span className={styles.separator}></span>
+            <span className={styles.totalCount}>6</span>
+          </div>
+        </section>
+        <section className={styles.outro}>
+          <p>Outro</p>
+        </section>
+      </div>
+    </main>
   );
 }
