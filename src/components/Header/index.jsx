0d8687aeb684be2e5c648da0c48f8f08e299f060
Rodrigo Santos <rrc.santos@campus.fct.unl.pt>
2024-11-18 16:31:20 +0000

    Removed the all the unwanted effects.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,8 +4,6 @@ import styles from "./style.module.scss";
 import { usePathname } from "next/navigation";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Rounded from "../../common/RoundedButton";
-import Magnetic from "../../common/Magnetic";
 
 export default function index() {
   const header = useRef(null);
@@ -54,24 +52,15 @@ export default function index() {
           </div>
         </div>
         <div className={styles.nav}>
-          <Magnetic>
-            <div className={styles.el}>
-              <a>Work</a>
-              <div className={styles.indicator}></div>
-            </div>
-          </Magnetic>
-          <Magnetic>
-            <div className={styles.el}>
-              <a>About</a>
-              <div className={styles.indicator}></div>
-            </div>
-          </Magnetic>
-          <Magnetic>
-            <div className={styles.el}>
-              <a>Contact</a>
-              <div className={styles.indicator}></div>
-            </div>
-          </Magnetic>
+          <div className={styles.el}>
+            <a>Work</a>
+          </div>
+          <div className={styles.el}>
+            <a>About</a>
+          </div>
+          <div className={styles.el}>
+            <a>Contact</a>
+          </div>
         </div>
       </div>
     </>
