7f33f8f09c85ae9f4ea0802e987bb6f279a051bd
Rodrigo Santos <rrc.santos@campus.fct.unl.pt>
2024-11-18 16:22:52 +0000

    New Boilerplate.

---
+++ src/app/fonts/fonts.js
@@ -0,0 +1,21 @@
+import localFont from "next/font/local";
+
+export const regularFont = localFont({
+  src: "./Editorial.otf",
+  variable: "--font-regular",
+});
+
+export const italicFont = localFont({
+  src: "./GreatVibes.ttf",
+  variable: "--font-italic",
+});
+
+export const pixelFont = localFont({
+  src: "./Mondwest.otf",
+  variable: "--font-pixel",
+});
+
+export const geistMono = localFont({
+  src: "./GeistMono.otf",
+  variable: "--font-mono",
+});
