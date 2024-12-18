d28c90b0beb035377909a13d6a7f0dcc6bbf0962
Rodrigo Santos <rrc.santos@campus.fct.unl.pt>
2024-12-18 21:28:52 +0000

    Update blog page: change label from "notes" to "blog" for improved clarity in content presentation.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -18,7 +18,7 @@ export default async function BlogPage() {
         <div className="flex flex-col items-center justify-center space-y-4 sm:text-center mb-20">
           <div className="space-y-2">
             <div className="sm:inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm hidden">
-              notes
+              blog
             </div>
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
               I like to learn
