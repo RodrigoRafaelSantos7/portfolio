1c3f6a073eff462829719e27282cc8b0b964b20e
RodrigoRafaelSantos7 <rodrigorafaelsantos7@icloud.com>
2025-02-21 11:43:54 +0000

    Add: page animations with Motion and create animation constants

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,32 +1,49 @@
+'use client'
+import { motion } from 'motion/react'
 import { TextEffect } from '@/components/ui/text-effect'
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
+import {
+  VARIANTS_CONTAINER,
+  VARIANTS_SECTION,
+  TRANSITION_SECTION,
+} from '@/constants/animations'
 
 export function Header() {
   return (
-    <header className='mb-8 flex items-center justify-start gap-4'>
-      <Image
-        src='/profile.png'
-        alt='Rodrigo Santos'
-        width={400}
-        height={400}
-        className='h-16 w-16 rounded-full hover:scale-105 transition-all duration-300'
-      />
-      <div>
-        <Link href='/' className='font-medium text-black dark:text-white'>
-          Rodrigo Santos
-        </Link>
-        <TextEffect
-          as='p'
-          preset='fade'
-          per='char'
-          className='text-zinc-600 dark:text-zinc-500 font-[family-name:var(--font-geist-mono)]'
-          delay={0.5}
-        >
-          Software Engineer
-        </TextEffect>
-      </div>
-    </header>
+    <motion.main
+      variants={VARIANTS_CONTAINER}
+      initial='hidden'
+      animate='visible'
+    >
+      <motion.header
+        className='mb-8 flex items-center justify-start gap-4'
+        variants={VARIANTS_SECTION}
+        transition={TRANSITION_SECTION}
+      >
+        <Image
+          src='/profile.png'
+          alt='Rodrigo Santos'
+          width={400}
+          height={400}
+          className='h-16 w-16 rounded-full hover:scale-105 transition-all duration-300'
+        />
+        <div>
+          <Link href='/' className='font-medium text-black dark:text-white'>
+            Rodrigo Santos
+          </Link>
+          <TextEffect
+            as='p'
+            preset='fade'
+            per='char'
+            className='text-zinc-600 dark:text-zinc-500 font-[family-name:var(--font-geist-mono)]'
+            delay={0.5}
+          >
+            Software Engineer
+          </TextEffect>
+        </div>
+      </motion.header>
+    </motion.main>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+'use client'
+import { motion } from 'motion/react'
+import {
+  VARIANTS_CONTAINER,
+  VARIANTS_SECTION,
+  TRANSITION_SECTION,
+} from '@/constants/animations'
+
+export default function Personal() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <motion.main
+      className='space-y-24'
+      variants={VARIANTS_CONTAINER}
+      initial='hidden'
+      animate='visible'
+    >
+      <motion.section
+        variants={VARIANTS_SECTION}
+        transition={TRANSITION_SECTION}
+      >
+        <div className='flex-1'>
+          <p className='text-zinc-600 dark:text-zinc-400'>
+            Focused on creating intuitive and performant web experiences.
+            Bridging the gap between design and development.
+          </p>
+        </div>
+      </motion.section>
+    </motion.main>
   )
 }
