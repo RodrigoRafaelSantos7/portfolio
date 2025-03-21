83535e0a57af197e9de780a7d023f1718949f1ad
RodrigoRafaelSantos7 <rodrigorafaelsantos7@icloud.com>
2025-03-21 14:43:18 +0000

    Initial commit for the make over

---
+++ app/layout.tsx
@@ -0,0 +1,52 @@
+import type { Metadata, Viewport } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { Header } from './header'
+import { Footer } from './footer'
+import { ThemeProvider } from 'next-themes'
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: '#ffffff',
+}
+
+export const metadata: Metadata = {
+  title: 'Nim - Personal website template',
+  description:
+    'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
+}
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${inter.className} bg-[#fdfdfc] tracking-tight antialiased dark:bg-[#111110]`}
+      >
+        <ThemeProvider
+          enableSystem={true}
+          attribute="class"
+          storageKey="theme"
+          defaultTheme="system"
+        >
+          <div className="flex min-h-screen w-full flex-col">
+            <div className="relative mx-auto w-full max-w-[692px] flex-1 px-6 py-12 md:py-16">
+              <Header />
+              {children}
+              <Footer />
+            </div>
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
